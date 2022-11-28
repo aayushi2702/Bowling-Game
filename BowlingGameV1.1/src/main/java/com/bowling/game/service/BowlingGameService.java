e14c0033a9247f208184c3c3d6d0c40cb138ec0b
HP <HP@DESKTOP-KQA8LBN>
2022-11-28 13:19:25 +0530

    Test : Case when a strike occur in first frame and then only one pin downs by each ball in each frame.

--- BowlingGameV1.1/src/main/java/com/bowling/game/service/BowlingGameService.java
+++ BowlingGameV1.1/src/main/java/com/bowling/game/service/BowlingGameService.java
@@ -13,13 +13,16 @@ public class BowlingGameService {
 		int score = 0;
 		int roll = 0;
 		for (int frame = 0; frame < 10; frame++) {
-			if (isSpare(roll)) {
-				score += 10 + rolls[roll + 2];
-				roll += 2;
-			} else {
-				score += rolls[roll] + rolls[roll + 1];
-				roll += 2;
-			}
+			if(isStrike(rolls[roll])) {
+                score += 10 + rolls[roll+1] + rolls[roll+2];
+                roll++;
+            } else if(isSpare(roll)) {
+                score += 10 + rolls[roll+2];
+                roll += 2;
+            } else {
+                score += rolls[roll] + rolls[roll+1];
+                roll += 2;
+            }
 		}
 		return score;
 	}
@@ -27,4 +30,9 @@ public class BowlingGameService {
 	private boolean isSpare(int roll) {
 		return rolls[roll] + rolls[roll + 1] == 10;
 	}
+	
+	private boolean isStrike(int roll) {
+        return roll == 10;
+    }
+
 }
