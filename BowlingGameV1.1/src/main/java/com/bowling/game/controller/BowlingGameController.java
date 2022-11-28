83547144d5df4a1fc4609637e7a94d7810a1ea0f
Ayushi <HP@DESKTOP-KQA8LBN>
2022-11-28 12:10:54 +0530

    Refactor :Controllers to use method in Service Class and to increase the readability.

--- BowlingGameV1.1/src/main/java/com/bowling/game/controller/BowlingGameController.java
+++ BowlingGameV1.1/src/main/java/com/bowling/game/controller/BowlingGameController.java
@@ -4,11 +4,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bowling.game.service.BowlingGameService;
+
 @RestController
 public class BowlingGameController {
 
+    private BowlingGameService bowlingGameService;
+
+    public BowlingGameController(BowlingGameService bowlingGameService) {
+        this.bowlingGameService = bowlingGameService;
+    }
+
+	
     @GetMapping(path = "/getScore")
     public int gameHandler(@RequestParam int[] game) {
-        return 0;
+        return bowlingGameService.getGameScore();
     }
 }
--- BowlingGameV1.1/src/test/java/com/bowling/game/controller/BowlingGameControllerTests.java
+++ BowlingGameV1.1/src/test/java/com/bowling/game/controller/BowlingGameControllerTests.java
@@ -1,5 +1,6 @@
 package com.bowling.game.controller;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -7,8 +8,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import com.bowling.game.service.BowlingGameService;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
@@ -17,9 +21,13 @@ public class BowlingGameControllerTests {
 	@Autowired
 	private MockMvc mockMvc;
 
+	@MockBean
+	private BowlingGameService bowlingGameService;
+
 	@Test
 	public void gameAPITest() throws Exception {
 
+		when(bowlingGameService.getGameScore()).thenReturn(0);
 		mockMvc.perform(get("/getScore?game=0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0"))
 				.andExpect(status().isOk());
 	}
