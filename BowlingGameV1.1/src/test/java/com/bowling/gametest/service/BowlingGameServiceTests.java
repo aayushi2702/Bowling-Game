740d1bbb2445192a87c3207a45614ef7c1d4ddb7
Ayushi <HP@DESKTOP-KQA8LBN>
2022-11-28 12:34:03 +0530

    Refactor : Creating instance for all test case in setUp method to increase readability.

--- BowlingGameV1.1/src/test/java/com/bowling/gametest/service/BowlingGameServiceTests.java
+++ BowlingGameV1.1/src/test/java/com/bowling/gametest/service/BowlingGameServiceTests.java
@@ -2,6 +2,7 @@ package com.bowling.gametest.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -10,17 +11,22 @@ import com.bowling.game.service.BowlingGameService;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BowlingGameServiceTests {
+	
+    private BowlingGameService bowlingGameService;
+
+    @Before
+    public void setUp() {
+        bowlingGameService = new BowlingGameService();
+    }
 
 	@Test
 	public void getGameScore_ReturnsValueTest() {
-		BowlingGameService bowlingGameService = new BowlingGameService();
 		assertThat(bowlingGameService
 				.getGameScore(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 })).isZero();
 	}
 	
     @Test
-    public void getGameScore_WithEachRollPinsDownAre1Test() {
-    	BowlingGameService bowlingGameService = new BowlingGameService();
+    public void getGameScore_WithEachRollPinsDownAreOneTest() {
         assertThat(bowlingGameService.getGameScore(new int[] {1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1}))
                 .isEqualTo(20);
     }
